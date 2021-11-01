8254db7ca83d5e32d45d30c85e0d754cbabf9a10
Breakfast34 <82320414+Breakfast34@users.noreply.github.com>
2021-11-01 21:56:34 +0700

    up date ค้นหา หน้า dashboard

--- Flutter-register-login-UI-main/lib/Screens/dashboard/search.dart
+++ Flutter-register-login-UI-main/lib/Screens/dashboard/search.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:login_ui/Animation/Fade_Animation.dart';
 import 'package:login_ui/Themes/Themes.dart';
+
+import 'package:login_ui/model/Search.dart';
 
 class Search extends StatelessWidget {
   @override
@@ -9,24 +12,38 @@ class Search extends StatelessWidget {
       1.2,
       Padding(
         padding: EdgeInsets.symmetric(horizontal: 15.0),
-        child: TextField(
-            cursorColor: PrimaryColor,
+        child: TypeAheadField(
+          hideSuggestionsOnKeyboardHide: false,
+          textFieldConfiguration: TextFieldConfiguration(
+            autofocus: true,
+            style: DefaultTextStyle.of(context).style.copyWith(fontSize: 14),
             decoration: InputDecoration(
-              fillColor: PrimaryColor,
-              prefixIcon: Icon(
-                Icons.search,
-                color: PrimaryColor,
-              ),
-              hintText: "ค้นหาที่นี่",
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30.0),
-                borderSide: BorderSide(color: PrimaryColor),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30.0),
-                borderSide: BorderSide(color: PrimaryColor),
-              ),
-            )),
+                enabled: false,
+                prefixIcon: Icon(Icons.search_outlined),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(color: PrimaryColor),
+                ),
+                disabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: PrimaryColor),
+                    borderRadius: BorderRadius.circular(20.0)),
+                hintText: 'ค้นหาที่นี้'),
+          ),
+          suggestionsCallback: (Pattern) async {
+            return await BackendService.getSuggestions(Pattern);
+          },
+          itemBuilder: (context, suggestion) {
+            return ListTile(
+              leading: Icon(Icons.shopping_cart),
+              title: Text(suggestion['name']),
+              subtitle: Text('\$${suggestion['price']}'),
+            );
+          },
+          onSuggestionSelected: (suggestion) {
+            // Navigator.of(context).push(MaterialPageRoute(
+            //     builder: (context) => ProductPage(product: suggestion)));
+          },
+        ),
       ),
     );
   }
---
+++ Flutter-register-login-UI-main/lib/model/Search.dart
@@ -0,0 +1,11 @@
+import 'dart:math';
+
+class BackendService {
+  static Future<List> getSuggestions(String query) async {
+    await Future.delayed(Duration(seconds: 1));
+
+    return List.generate(3, (index) {
+      return {'name': query + index.toString(), 'price': Random().nextInt(100)};
+    });
+  }
+}
