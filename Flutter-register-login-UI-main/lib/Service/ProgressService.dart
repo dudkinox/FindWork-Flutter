808ba9975898505b3a876b2955ffa83b970ef18c
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-11-06 15:38:30 +0700

    Update ProgressService.dart

--- Flutter-register-login-UI-main/lib/Service/ProgressService.dart
+++ Flutter-register-login-UI-main/lib/Service/ProgressService.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names, missing_return
+
 import 'dart:convert';
 
 import 'package:login_ui/Database/Host.dart';
@@ -14,4 +16,27 @@ Future<List<JobDataModel>> Progress(String token) async {
   );
   List jsonResponse = json.decode(response?.body);
   return jsonResponse.map((data) => new JobDataModel.fromJson(data)).toList();
+}
+
+Future<List<JobDataModel>> AddProgress(String token, String job_id) async {
+  try {
+    final String url = Host + "/api/login/" + token;
+    final response = await http.put(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        "job_id": job_id,
+      }),
+    );
+    if (response.statusCode == 400) {
+      var err = json.decode(json.encode(response.body));
+      return err;
+    }
+    var data = json.decode(json.encode(response.body));
+    return data;
+  } catch (e) {
+    print(e);
+  }
 }
