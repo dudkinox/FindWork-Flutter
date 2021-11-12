e6e3161c35371bdd403cda1bcd4ef649ab37b442
Breakfast34 <82320414+Breakfast34@users.noreply.github.com>
2021-11-12 16:30:47 +0700

    up showResume Zoom In/Out

--- Flutter-register-login-UI-main/lib/ScreensAddjob/managerJob/showResume.dart
+++ Flutter-register-login-UI-main/lib/ScreensAddjob/managerJob/showResume.dart
@@ -9,6 +9,8 @@ class showResume extends StatefulWidget {
 }
 
 class _showResumeState extends State<showResume> {
+  final double minScale = 1;
+  final double maxScale = 4;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -27,15 +29,19 @@ class _showResumeState extends State<showResume> {
             backgroundColor: PrimaryColor,
           ),
           body: Container(
-            width: MediaQuery.of(context).size.width * 1,
-            height: MediaQuery.of(context).size.height * 1,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(DefaultImage),
-                  fit: BoxFit.cover,
-                ),
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(15.0)),
+            padding: EdgeInsets.all(20),
+            child: InteractiveViewer(
+                clipBehavior: Clip.none,
+                panEnabled: false,
+                minScale: minScale,
+                maxScale: maxScale,
+                child: AspectRatio(
+                    aspectRatio: 1,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.network(
+                            "https://ichef.bbci.co.uk/news/800/cpsprodpb/1124F/production/_119932207_indifferentcatgettyimages.png",
+                            fit: BoxFit.cover)))),
           ),
         ),
       ),
