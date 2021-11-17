bc9f3b5a341f4b5a83191354f5d3d77b1d813e12
Tossawat <51531950+tanza4432@users.noreply.github.com>
2021-11-17 23:38:12 +0700

    Update SendMessage

--- Flutter-register-login-UI-main/lib/Service/MessageService.dart
+++ Flutter-register-login-UI-main/lib/Service/MessageService.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
@@ -14,9 +12,43 @@ Future<dynamic> GetMessage(String token) async {
       'Content-Type': 'application/json; charset=UTF-8',
     },
   );
-  if(response.body == "หาไม่เจอ เส้น /massage/:id_user"){
+  if (response.body == "หาไม่เจอ เส้น /massage/:id_user") {
     return "หาไม่เจอ";
   }
   List jsonResponse = json.decode(response?.body);
   return jsonResponse.map((data) => new MessageModel.fromJson(data)).toList();
+}
+
+Future<String> SendApproveMessage(
+  String job_id,
+  String id_user,
+  String msg
+) async {
+  try {
+    final String Url = Host + "/api/msg";
+    final response = await http.post(
+      Uri.parse(Url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        "job_id": [
+          {
+            "id": job_id,
+            "message": msg,
+          }
+        ],
+        "id_user": id_user
+      }),
+    );
+
+    if (response.statusCode == 400) {
+      var err = json.decode(json.encode(response.body));
+      return err;
+    }
+    var data = json.decode(json.encode(response.body));
+    return data;
+  } catch (e) {
+    print(e);
+  }
 }
